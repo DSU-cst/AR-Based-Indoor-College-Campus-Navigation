c2fb481434e186f1c102a70fb3b01eb21b78fdca
CapstoneProjectDSU <149241928+CapstoneProjectDSU@users.noreply.github.com>
2023-11-29 01:16:57 +0530

    Add files via upload

--- Assets/Scripts/SetNavigationTarget.cs
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -1,32 +1,52 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class SetNavigationTarget : MonoBehaviour {
+public class SetNavigationTarget : MonoBehaviour
+{
 
     [SerializeField]
-    private Camera topDownCamera;
+    private TMP_Dropdown navigationTargetDropdown;
     [SerializeField]
-    private GameObject navTargetObject;
+    private List<Target> navigationTargetObjects = new List<Target>();
 
     private NavMeshPath path; // current calculated path 
     private LineRenderer line; // linerenderer to display path
-    private bool lineToggle = false; 
+    private Vector3 targetPosition = Vector3.zero; //current target position
 
-    private void Start() {
+    private bool lineToggle = false;
+
+    private void Start()
+    {
         path = new NavMeshPath();
         line = transform.GetComponent<LineRenderer>();
+        line.enabled = lineToggle;
     }
-    private void Update() {
-        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)) { 
-            lineToggle = !lineToggle; 
-        }
-        if (lineToggle) {
-            NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
+    private void Update()
+    {
+        if (lineToggle && targetPosition != Vector3.zero)
+        {
+            NavMesh.CalculatePath(transform.position, targetPosition, NavMesh.AllAreas, path);
             line.positionCount = path.corners.Length;
             line.SetPositions(path.corners);
-            line.enabled = true;
         }
+    }
+
+    public void SetCurrentNavigationTarget(int selectedValue)
+    {
+        targetPosition = Vector3.zero;
+        string selectedText = navigationTargetDropdown.options[selectedValue].text;
+        Target currentTarget = navigationTargetObjects.Find(x => x.Name.Equals(selectedText));
+        if (currentTarget != null)
+        {
+            targetPosition = currentTarget.PositionObject.transform.position;
+        }
+    }
+
+    public void ToogleVisibility()
+    {
+        lineToggle = !lineToggle;
+        line.enabled = lineToggle;
     }
 }
